680e5c05a6325e598525fde8f3d2093b716f64fe
P-Moseley <moseley.patrick0@gmail.com>
2021-09-29 20:49:50 +0100

    Finished All Blocks

--- partials/contact.php
+++ partials/contact.php
@@ -3,8 +3,9 @@ $contact_title = get_sub_field('contact_title');
 $contact_content = get_sub_field('contact_content');
 ?>
 <section class="contact_section">
-    <div class="content_section-container">
+    <div class="contact_section-container">
         <div class="contact_section-title"><?=$contact_title?></div>
         <div class="contact_section-content"><?=$contact_content?></div>
     </div>
+    <div class="btn">Contact Us</div>
 </section>
