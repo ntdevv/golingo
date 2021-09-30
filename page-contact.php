0d7665ddaa97d7079ccdc71bb7f561b62ba76771
P-Moseley <moseley.patrick0@gmail.com>
2021-09-30 21:34:33 +0100

    Contact page masthead

--- page-contact.php
+++ page-contact.php
@@ -2,11 +2,13 @@
 get_header();
 ?>
 
-<section class="contact_page">
-    <div class="contact_page-masthead" style="">
+<section class="contact_page"  style="background-image: url(https://cdn.pixabay.com/photo/2018/08/12/16/59/parrot-3601194_960_720.jpg);background-size:cover;">
+    <div class="contact_page-masthead">
         <div class="contact_page-masthead-container">
             <div class="contact_page-masthead-text">
-                <div class="contact_page-masthead-text-title">
+                    <div class="contact_page-masthead-overlay" style="position:absolute; top:0; left:0;width:100%; height:100%;z-index:2;background-color:black; mix-blend-mode:overlay;"></div>
+                    <div class="contact_page-masthead-text-title" style="z-index:3; color:white; font-size:3rem">
+
                     We Would Love To Hear From You
                 </div>
             </div>
--- partials/cf7_block.php
+++ partials/cf7_block.php
@@ -1,9 +1,12 @@
 <?php
 $cf7_shortcode = get_sub_field('cf7-shortcode');
+$left_image = get_sub_field('contact_form_img');
 ?>
-<section class="contact_section">
-    <div class="contact_section-container">
-        <div class="contact_section-content"><?=do_shortcode( $cf7_shortcode, false )?></div>
+<section class="contact_form_section">
+    <div class="contact_form_section-container">
+        <div class="contact_form_section-content"><?=do_shortcode( $cf7_shortcode, false )?></div>
+        <div class="contact_form_section-image">
+            <img src="<?=$left_image?>" alt="">
+        </div>
     </div>
-    <div class="btn">Contact Us</div>
 </section>
